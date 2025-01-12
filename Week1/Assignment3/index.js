cd0cc22a1beb10c838aff798027215f665ccf8af
Adamb7rr <bahrcr7@gmail.com>
2025-01-12 18:58:23 +0200

    Assign3

--- Week1/Assignment3/index.js
+++ Week1/Assignment3/index.js
@@ -1,5 +1,6 @@
+
 console.log('%celzero %cweb %cschool',
-            'font-size: 40px; color: red;',
-            'font-size: 40px; color: green; font-weight: bold;',
-            'font-size: 40px; background-color: blue; color: #FFF;'
-);
+    'font-size: 40px; color: red;',
+    'font-size: 40px; color: green; font-weight: bold;',
+    'font-size: 40px; background-color: blue; color: #FFF'
+)
