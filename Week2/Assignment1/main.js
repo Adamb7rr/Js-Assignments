d718d06fd4737285db3c388e7fc02810a1775bfd
Adamb7rr <bahrcr7@gmail.com>
2025-01-13 13:42:06 +0200

    Assign1

--- Week2/Assignment1/main.js
+++ Week2/Assignment1/main.js
@@ -1,12 +1,16 @@
 // Add Variables Here
+// let Number_One = 10;
+// let Number_Two = 20;
+
 let Number_One = 10;
 let Number_Two = 20;
 
 // Ouput
 console.log(Number_One + '' + Number_Two) // Normal Concatenate => 1020
-console.log(typeof (Number_One + '' + Number_Two)) // Normal Concatenate => String
-console.log(`${Number_One}${Number_Two}`) // Template Literals Way => 1020
+console.log(typeof (Number_One + '' + Number_Two))// Normal Concatenate => String
+console.log(`${Number_One}${Number_Two}`)// Template Literals Way => 1020
 console.log(typeof `${Number_One}${Number_Two}`) // Template Literals Way => String
+
 console.log(Number_One + '\n' +
     Number_Two)
 /*
