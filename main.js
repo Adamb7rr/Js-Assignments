822174a8e1bfcb0971093ea2b8ee8e0a0a4f1ee5
Adamb7rr <bahrcr7@gmail.com>
2025-01-27 12:47:26 +0200

    Lesson 1

---
+++ main.js
@@ -0,0 +1,30 @@
+// document.body.innerHTML = 'Hello';
+// console.log(10 - 7);
+// console.log(alert("Good Morning!"))
+// console.log(alert("Adam Abdelafeth"))
+// console.log(10 + 5)
+// console.log(20 - 5)
+// console.log(2 + 2 - 5)
+// document.body.innerHTML = 'Good Morning';
+// document.body.innerHTML = 'Adam';
+
+// challenges 1
+
+// let Tshirt = 10;
+// let socks = 8;
+// let plates = 20;
+// let sum = Tshirt + socks + plates;
+// console.log(sum + '$');
+
+// // challenges 2
+
+// let bank = 100;
+// let lunch = 20;
+// let dinner = 50;
+// let earn = 200;
+// let money = (bank - lunch - dinner) + 200;
+// console.log(money + '$');
+
+// // challenges 3
+
+// document.body.innerHTML = '';
