26f56e202e5592a18f61f95f6b3f83da06ed6a3d
Adamb7rr <bahrcr7@gmail.com>
2025-01-29 19:48:27 +0200

    Exercise

--- main.js
+++ main.js
@@ -165,12 +165,21 @@
 // let myMove = '';
 
 
-let hour = 7
+// let hour = 7
+// let name = 'Adam'
 
-if (hour >= 6 && hour <= 12) {
-    console.log("Good Morning!")
-} else if (hour >= 13 && hour <= 17) {
-    console.log("Good Afternoon!")
+// if (hour >= 6 && hour <= 12) {
+//     console.log(`Good Morning! ${name}`)
+// } else if (hour >= 13 && hour <= 17) {
+//     console.log(`Good Afternoon! ${name}`)
+// } else {
+//     console.log(`Good Night! ${name}`)
+// }
+
+let age = 22
+
+if (age >= 1 && age <= 6 || age >= 65) {
+    console.log("Discount")
 } else {
-    console.log("Good Night!")
+    console.log("No Discount")
 }
