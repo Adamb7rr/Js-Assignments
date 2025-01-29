4e70670deee7056386022cfd8440fb25b99b1e5f
Adamb7rr <bahrcr7@gmail.com>
2025-01-29 19:39:57 +0200

    Exercise

--- main.js
+++ main.js
@@ -163,3 +163,14 @@
 
 // let comuterMove = '';
 // let myMove = '';
+
+
+let hour = 7
+
+if (hour >= 6 && hour <= 12) {
+    console.log("Good Morning!")
+} else if (hour >= 13 && hour <= 17) {
+    console.log("Good Afternoon!")
+} else {
+    console.log("Good Night!")
+}
