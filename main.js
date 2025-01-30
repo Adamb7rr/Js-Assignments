4e86cccbe69f82ceb61512f0cdec654ab4c40f3b
Adamb7rr <bahrcr7@gmail.com>
2025-01-30 21:26:59 +0200

    Exercise

--- main.js
+++ main.js
@@ -385,11 +385,16 @@ function pickComputerMove () {
     return comuterMove;
 }
 
-const score = {
+let score = JSON.parse(localStorage.getItem
+    ('score')) || {
     wins: 0,
     losses: 0,
     ties: 0
-}
+};
+
+
+
+
 
 function playGame (myMove) {
     const computerMove = pickComputerMove();
@@ -429,6 +434,10 @@ function playGame (myMove) {
         score.ties += 1
     }
 
+    localStorage.setItem('score', JSON.stringify(score))
+
+    // console.log(localStorage.setItem('score', JSON.stringify('score')))
+
     alert(`You Choose ${myMove} and Computer choose ${computerMove}. ${res}.
 Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`)
 }
@@ -437,6 +446,7 @@ function reset () {
     score.wins = 0
     score.losses = 0
     score.ties = 0
+    localStorage.removeItem('score')
 
     alert('Game Was Reset Scores')
 }
