2bccf0f2090a577fd37d302b47762104676fea02
Adamb7rr <bahrcr7@gmail.com>
2025-02-06 00:01:36 +0200

    Exercise

--- main.js
+++ main.js
@@ -500,131 +500,140 @@
 // document.body.innerHTML = 'Adam'
 // document.title = 'Training'
 
-let subscribeButton = document.querySelector('.Subs')
+// let subscribeButton = document.querySelector('.Subs')
 
-let changeColor = document.querySelector('.Subs')
+// let changeColor = document.querySelector('.Subs')
 
-console.log(subscribeButton)
+// console.log(subscribeButton)
 
-function change () {
-    if (subscribeButton.innerHTML === 'Subscribe') {
-        subscribeButton.innerHTML = 'Subscribed'
-        changeColor.style = 'background-color: #EEE; color: #333'
-    } else if (subscribeButton.innerHTML === 'Subscribed') {
-        subscribeButton.innerHTML = 'Subscribe'
-        changeColor.style = ''
-    }
-    // subscribeButton = 'Subscribed'
-}
+// function change () {
+//     if (subscribeButton.innerHTML === 'Subscribe') {
+//         subscribeButton.innerHTML = 'Subscribed'
+//         changeColor.style = 'background-color: #EEE; color: #333'
+//     } else if (subscribeButton.innerHTML === 'Subscribed') {
+//         subscribeButton.innerHTML = 'Subscribe'
+//         changeColor.style = ''
+//     }
+//     // subscribeButton = 'Subscribed'
+// }
 
-function pickComputerMove () {
-    let randomNumber = Math.random();
+// function pickComputerMove () {
+//     let randomNumber = Math.random();
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        comuterMove = 'Rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        comuterMove = 'Paper';
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        comuterMove = 'Scissors';
-    }
-    return comuterMove;
-}
+//     if (randomNumber >= 0 && randomNumber < 1 / 3) {
+//         comuterMove = 'Rock';
+//     } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+//         comuterMove = 'Paper';
+//     } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+//         comuterMove = 'Scissors';
+//     }
+//     return comuterMove;
+// }
 
-let score = JSON.parse(localStorage.getItem
-    ('score')) || {
-    wins: 0,
-    losses: 0,
-    ties: 0
-};
+// let score = JSON.parse(localStorage.getItem
+//     ('score')) || {
+//     wins: 0,
+//     losses: 0,
+//     ties: 0
+// };
 
-let rock = document.querySelector('.rock')
+// let rock = document.querySelector('.rock')
 
-console.log(rock.innerHTML)
+// console.log(rock.innerHTML)
 
-let message = document.querySelector('.show')
-let sc = document.querySelector('.score')
+// let message = document.querySelector('.show')
+// let sc = document.querySelector('.score')
 
-function playGame (myMove) {
-    const computerMove = pickComputerMove();
-    let res = '';
+// function playGame (myMove) {
+//     const computerMove = pickComputerMove();
+//     let res = '';
 
-    if (myMove === "Rock") {
-        if (computerMove === 'Paper') {
-            res = 'You Lose'
-        } else if (computerMove === 'Scissors') {
-            res = 'You Win'
-        } else if (computerMove === 'Rock') {
-            res = 'Tie'
-        }
-    } else if (myMove === "Paper") {
-        if (computerMove === 'Paper') {
-            res = 'Tie'
-        } else if (computerMove === 'Scissors') {
-            res = 'You Lose'
-        } else if (computerMove === 'Rock') {
-            res = 'You Win'
-        }
-    } else if (myMove === "Scissors") {
-        if (computerMove === 'Paper') {
-            res = 'You Win'
-        } else if (computerMove === 'Scissors') {
-            res = 'Tie'
-        } else if (computerMove === 'Rock') {
-            res = 'You Lose'
-        }
-    }
+//     if (myMove === "Rock") {
+//         if (computerMove === 'Paper') {
+//             res = 'You Lose'
+//         } else if (computerMove === 'Scissors') {
+//             res = 'You Win'
+//         } else if (computerMove === 'Rock') {
+//             res = 'Tie'
+//         }
+//     } else if (myMove === "Paper") {
+//         if (computerMove === 'Paper') {
+//             res = 'Tie'
+//         } else if (computerMove === 'Scissors') {
+//             res = 'You Lose'
+//         } else if (computerMove === 'Rock') {
+//             res = 'You Win'
+//         }
+//     } else if (myMove === "Scissors") {
+//         if (computerMove === 'Paper') {
+//             res = 'You Win'
+//         } else if (computerMove === 'Scissors') {
+//             res = 'Tie'
+//         } else if (computerMove === 'Rock') {
+//             res = 'You Lose'
+//         }
+//     }
 
-    if (res === 'You Win') {
-        score.wins += 1
-    } else if (res === 'You Lose') {
-        score.losses += 1
-    } else if (res === 'Tie') {
-        score.ties += 1
-    }
+//     if (res === 'You Win') {
+//         score.wins += 1
+//     } else if (res === 'You Lose') {
+//         score.losses += 1
+//     } else if (res === 'Tie') {
+//         score.ties += 1
+//     }
 
-    localStorage.setItem('score', JSON.stringify(score))
+//     localStorage.setItem('score', JSON.stringify(score))
 
-    // console.log(localStorage.setItem('score', JSON.stringify('score')))
+//     // console.log(localStorage.setItem('score', JSON.stringify('score')))
 
-    //     alert(`You Choose ${myMove} and Computer choose ${computerMove}. ${res}.
-    // Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`)
+//     //     alert(`You Choose ${myMove} and Computer choose ${computerMove}. ${res}.
+//     // Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`)
 
-    // message.innerHTML = `You Choose ${myMove} and Computer choose ${computerMove}.`
-    message.innerText = "You: " + myMove + '\n' + "Com: " + computerMove + '\n' + res
+//     // message.innerHTML = `You Choose ${myMove} and Computer choose ${computerMove}.`
+//     message.innerText = "You: " + myMove + '\n' + "Com: " + computerMove + '\n' + res
 
-    sc.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`
-}
-
-
-
-function reset () {
-    score.wins = 0
-    score.losses = 0
-    score.ties = 0
-    localStorage.removeItem('score')
-
-    // alert('Game Was Reset Scores')
-
-    message.innerHTML = `Score Reset`
-    sc.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`
-}
+//     sc.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`
+// }
 
 
 
-let price = document.querySelector('.price')
+// function reset () {
+//     score.wins = 0
+//     score.losses = 0
+//     score.ties = 0
+//     localStorage.removeItem('score')
 
-function calc () {
-    // price.innerHTML = `${cost_order}`
-    const cost_order = document.querySelector('.cost')
-    let ship = Number(cost_order.value);
-    console.log(typeof (Number(cost_order.value)))
-    console.log(cost_order.value)
-    if (ship <= 40) {
+//     // alert('Game Was Reset Scores')
 
-        ship = ship + 10
-        price.innerHTML = `$${ship} \t with $10 shipping.`
-    } else {
-        // ship = cost_order.value + 0
-        price.innerText = `$${ship} \t with FREE shipping.`
-    }
-}
+//     message.innerHTML = `Score Reset`
+//     sc.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`
+// }
+
+
+
+// let price = document.querySelector('.price')
+
+// function calc () {
+//     // price.innerHTML = `${cost_order}`
+//     const cost_order = document.querySelector('.cost')
+//     let ship = Number(cost_order.value);
+//     console.log(typeof (Number(cost_order.value)))
+//     console.log(cost_order.value)
+//     if (ship <= 40) {
+
+//         ship = ship + 10
+//         price.innerHTML = `$${ship} \t with $10 shipping.`
+//     } else {
+//         // ship = cost_order.value + 0
+//         price.innerText = `$${ship} \t with FREE shipping.`
+//     }
+// }
+
+// function handleKey (event) {
+//     if (event.key === 'Enter') {
+//         calc()
+//     }
+// }
+
+let but = document.querySelector('button')
+console.log(but)
