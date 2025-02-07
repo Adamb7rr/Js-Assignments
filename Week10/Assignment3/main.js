b9d20659a6079c6ff8d311b46dae5a60cd76431e
Adamb7rr <bahrcr7@gmail.com>
2025-02-07 17:57:49 +0200

    Exercise

--- Week10/Assignment3/main.js
+++ Week10/Assignment3/main.js
@@ -3,11 +3,18 @@
 // const jsButton3 = document.querySelector('.js-button3')
 function check (selector) {
     const jsButton = document.querySelector(selector)
-    if (jsButton.classList.contains('is-toggled')) {
-        console.log('Exist')
-        jsButton.classList.remove('is-toggled')
+    if (!jsButton.classList.contains('is-toggled')) {
+        turnOffPreviousButton();
+        jsButton.classList.add('is-toggled')
     } else {
         console.log('Not Exist')
-        jsButton.classList.add('is-toggled')
+        jsButton.classList.remove('is-toggled')
+    }
+}
+
+function turnOffPreviousButton () {
+    const previousButton = document.querySelector('.is-toggled');
+    if (previousButton) {
+        previousButton.classList.remove('is-toggled');
     }
 }
